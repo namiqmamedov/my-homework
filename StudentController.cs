4d5caa1b6e2b247c27abcf9466163cc38433c656
namiqmm <107476714+namiqmm@users.noreply.github.com>
2022-07-21 17:37:36 +0400

    code

---
+++ StudentController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Helpers;
+using DataAccess.Repositories.Implementations;
+
+namespace Console_App.Controller
+{
+
+    public class StudentController
+    {
+        private StudentRepository _studentRepository;
+        private GroupRepository _groupRepository;
+
+        public StudentController()
+        {
+            _studentRepository = new StudentRepository();
+        }
+        public void CreateStudent()
+        {
+            var groups = _groupRepository.GetAll();
+            if (groups.Count != 0)
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Magenta, "Enter student name:");
+                string name = Console.ReadLine();
+
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Magenta, "Enter student surname:");
+                string surname = Console.ReadLine();
+
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Magenta, "Enter student age:");
+                string age = Console.ReadLine();
+                byte studentAge;
+                bool result = byte.TryParse(age, out studentAge);
+
+
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Gray, "All groups");
+                foreach (var group in groups)
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, group.Name);
+                }
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Magenta, "Enter group name:");
+                string groupName = Console.ReadLine();
+
+                var dbGroup = _groupRepository.Get(g => g.Name.ToLower() == groupName.ToLower());
+
+                if (dbGroup != null)
+                {
+                    if (dbGroup.MaxSize > dbGroup.CurrentSize)
+                    {
+                        var student = new Student
+                        {
+                            Name = name,
+                            Surname = surname,
+                            Age = studentAge,
+                            Group = dbGroup,
+
+                        };
+                        dbGroup.CurrentSize++;
+
+                        _studentRepository.Create(student);
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, $"Name:{student.Name}, Surname:{student.Surname}, Age:{student.Age}, Group:{student.Group}");
+                    }
+                    else
+                    {
+                        {
+                            ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, $"Group is full, max size of group {dbGroup.MaxSize}");
+                        }
+                    }
+
+                }
+                else
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, $"Including group doesn't exist");
+                }
+
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, $"You have to create before creating of student");
+            }
+
+
+
+        }
+        
+        
+        
+        
+            
+    }
+
+
+
+}
+
+
+
